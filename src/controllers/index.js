f4d55d3d17954d69d43feb90d7a59e0b44170b22
dhirendra667 <dhyaduvanshi667@gmail.com>
2026-07-23 19:24:40 +0530

    Feat some changes done for face authentication

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,12 +4,3 @@ const subscriptionController = require('./subscription_controller');
 const faceAuthController = require('./face_auth_controller');
 
 module.exports = { authController, userController, subscriptionController, faceAuthController };
-
-
-
-// const authController = require('./auth_controller');
-// const userController = require('./user_controller');
-// const subscriptionController = require('./subscription_controller');
-// const face_auth_controller = require('/face_auth-controller');
-
-// module.exports = { authController, userController, subscriptionController , face_auth_controller};
--- src/services/index.js
+++ src/services/index.js
@@ -5,11 +5,3 @@ const SubscriptionService = require('./subscription_service');
 const FaceAuthService = require('./face_auth_service');
 
 module.exports = { AuthService, OtpService, UserService, SubscriptionService, FaceAuthService };
-
-
-// const AuthService = require('./auth_service');
-// const OtpService = require('./otp_service');
-// const UserService = require('./user_service');
-// const SubscriptionService = require('./subscription_service');
-
-// module.exports = { AuthService, OtpService, UserService, SubscriptionService };
